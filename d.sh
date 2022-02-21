c5822157242a2f81b3a1d2a8cb7d7f972cb35dab
Hesham-XQ <34285068+Hesham-XQ@users.noreply.github.com>
2022-02-21 17:30:18 +0200

    Create d.sh

---
+++ d.sh
@@ -0,0 +1,9 @@
+echo ----------------------- deployment start --------------------
+sleep 5
+echo ..
+echo 'loading artifacts'
+echo 'deploying on server 54'
+echo 'wait as deployment finish'
+echo 'remove artifacts'
+echo 'copying logfiles......'
+echo ------------------------- deployment successfull-----------------
